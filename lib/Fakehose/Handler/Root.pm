a5592d09dff52f56bb4b10e591878e92eff2ef2f
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-31 19:16:19 -0700

    welcome screen

--- lib/Fakehose/Handler/Root.pm
+++ lib/Fakehose/Handler/Root.pm
@@ -4,7 +4,17 @@ use parent qw(Tatsumaki::Handler);
 
 sub get {
     my $self = shift;
-    $self->write("Welcome to fakehose!");
+    my $env  = $self->request->env;
+    my $base = "http://$env->{SERVER_NAME}:$env->{SERVER_PORT}";
+    $self->write(<<HTML);
+<html>
+<head><title>Fakehose</title></head>
+<body>
+<h1>Congrats! Your Fakehose is running!</h1>
+<p>Your twitter streaming client can connect to $base/1/statuses/filter.json etc. for testing.</p>
+<p>Also you can POST arbitrary JSON to $base/_fakehose/inject to generate new events.</p>
+</body>
+HTML
 }
 
 1;
